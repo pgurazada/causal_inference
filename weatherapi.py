e16573b8aaa3c709fd2dd8febb67e726a572fc78
pgurazada <efpm04015@iiml.ac.in>
2020-02-28 05:23:53 +0000

    pull weather from API

---
+++ weatherapi.py
@@ -0,0 +1,25 @@
+import sys
+import requests
+import json
+
+API_KEY = sys.argv[1]
+CITY_NAME = sys.argv[2]
+
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+complete_url = (base_url +
+                "appid=" + API_KEY + 
+                "&q=" + CITY_NAME)
+
+response = requests.get(complete_url)
+
+response_json = response.json()
+
+if response_json["cod"] != "404":
+    data = response_json["main"]
+    current_temp = data["temp"]
+    current_rh = data["humidity"]
+else:
+    print("City not found")
+
+print(f"City: {CITY_NAME}, Temp: {current_temp}, RH: {current_rh}")
